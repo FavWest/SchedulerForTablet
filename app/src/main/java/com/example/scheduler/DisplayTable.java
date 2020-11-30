64998f48ff7c03b6ac6ab95ac7efbe17fdb9fae9
Tori West <Practiceandthenpractice@gmail.com>
2020-11-30 10:59:18 -0800

    latest

--- app/src/main/java/com/example/scheduler/DisplayTable.java
+++ app/src/main/java/com/example/scheduler/DisplayTable.java
@@ -6,6 +6,8 @@ import androidx.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import static com.example.scheduler.MainActivity.FRI;
