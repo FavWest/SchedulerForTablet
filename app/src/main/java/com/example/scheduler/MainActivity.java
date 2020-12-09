a428d70d6bc438970c426d4fb488b835fbed83c1
Tori <practiceandthenpractice@gmail.com>
2020-12-09 08:56:46 -0800

    minor changes

--- app/src/main/java/com/example/scheduler/MainActivity.java
+++ app/src/main/java/com/example/scheduler/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-//I added a comment! 11-30-2020
 public class MainActivity extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener {
 
